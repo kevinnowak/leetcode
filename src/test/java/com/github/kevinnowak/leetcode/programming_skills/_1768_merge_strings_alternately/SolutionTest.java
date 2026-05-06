7264cad5b874e7fe8fcc0a2c0e12998933a6980f
Kevin Nowak <kevin.nowak@studium.fernuni-hagen.de>
2026-05-06 19:17:24 +0200

    Update

---
+++ src/test/java/com/github/kevinnowak/leetcode/programming_skills/_1768_merge_strings_alternately/SolutionTest.java
@@ -0,0 +1,31 @@
+package com.github.kevinnowak.leetcode.programming_skills._1768_merge_strings_alternately;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.stream.Stream;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+class SolutionTest {
+
+    @ParameterizedTest
+    @MethodSource("provideDataForTestMergeAlternately")
+    void testmergeAlternately(String word1, String word2, String expectedResult) {
+        // Given
+        Solution solution = new Solution();
+
+        // When
+        var actualResult = solution.mergeAlternately(word1, word2);
+
+        // Then
+        assertEquals(expectedResult, actualResult);
+    }
+
+    private static Stream<Arguments> provideDataForTestMergeAlternately() {
+        return Stream.of(
+                Arguments.of("abc", "pqr", "apbqcr"),
+                Arguments.of("ab", "pqrs", "apbqrs"),
+                Arguments.of("abcd", "pq", "apbqcd"));
+    }
+}
---
+++ src/test/java/com/github/kevinnowak/leetcode/programming_skills/_389_find_the_difference/SolutionTest.java
@@ -0,0 +1,31 @@
+package com.github.kevinnowak.leetcode.programming_skills._389_find_the_difference;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.stream.Stream;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+class SolutionTest {
+
+    @ParameterizedTest
+    @MethodSource("provideDataForTestFindTheDifference")
+    void testFindTheDifference(String s, String t, char expectedResult) {
+        // Given
+        Solution solution = new Solution();
+
+        // When
+        var givenResult = solution.findTheDifference(s, t);
+
+        // Then
+        assertEquals(expectedResult, givenResult);
+    }
+
+    private static Stream<Arguments> provideDataForTestFindTheDifference() {
+        return Stream.of(
+                Arguments.of("abcd", "abcde", "e"),
+                Arguments.of("", "y", "y"),
+                Arguments.of("a", "aa", "a"));
+    }
+}
